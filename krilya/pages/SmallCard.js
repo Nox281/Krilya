6de6f149e0703db6f8f6cdd981c5b9212d68919f
Nox281 <rbahiayoub@gmail.com>
2023-03-22 16:11:43 +0000

    h

--- krilya/pages/SmallCard.js
+++ krilya/pages/SmallCard.js
@@ -3,17 +3,11 @@ import React from 'react'
 
 function SmallCard({img, name, model}) {
   return (
-    <div className="h-16 w-16">
-      <h1>
-        {name} {model}
-      </h1>
-      <Image
-        src="https://nox281.github.io/Krilya/krilya/public/fiesta.jpg"
-        layout="fill"
-        className="rounder-lg"
-      />
+    <div className='h-16 w-16'>
+      <h1>{name} {model}</h1>
+      <Image src={img} layout='fill' className='rounder-lg'/>
     </div>
-  );
+  )
 }
 
 export default SmallCard
